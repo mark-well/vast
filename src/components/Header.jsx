2f169b204d3caacd238f124c3a9fc54608769683
mark-well <lacemerto05@gmail.com>
2026-03-15 11:16:24 +0800

    ref: close searchbar and options when clicked outside

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -49,12 +49,14 @@ function Header({ type, title }) {
                 <>
                     <header className="bg-(--primary-color) text-white min-h-12 w-full gap-x-4 py-2 px-4 flex items-center justify-between">
                         <h1 className={`text-white font-bold text-2xl cursor-pointer ${isSearchBarHidden ? '' : 'hidden'}`}>VAST</h1>
-                        <input type="text" onChange={(e) => setSearchQuery(e.target.value)} placeholder='Search' className={`w-full ${isSearchBarHidden ? 'hidden' : ''} px-4 bg-white h-8 rounded-sm text-(--text-primary) text-base outline-none`} />
+                        <input type="text" onChange={(e) => setSearchQuery(e.target.value)} placeholder='Search' className={`w-full z-2 ${isSearchBarHidden ? 'hidden' : ''} px-4 bg-white h-8 rounded-sm text-(--text-primary) text-base outline-none`} />
+                        <div className={`${isSearchBarHidden ? 'hidden' : ''} backdrop absolute w-full h-full left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-1`} onClick={toggleSearchBar}></div>
                         <div className='flex'>
                             <Button onClick={toggleSearchBar} className="cursor-pointer" icon={<FontAwesomeIcon icon="fa-solid fa-magnifying-glass" size='xl' />} />
                             <Button onClick={toggleTooltip} className="cursor-pointer" icon={<FontAwesomeIcon icon="fa-solid fa-ellipsis-vertical" size='xl' />} />
                         </div>
-                        <TooltipMain className={`${isTooltipHidden ? 'hidden' : ''}`}></TooltipMain>
+                        <TooltipMain className={`${isTooltipHidden ? 'hidden' : ''} z-2`}></TooltipMain>
+                        <div className={`${isTooltipHidden ? 'hidden' : ''} backdrop absolute w-full h-full left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-1`} onClick={toggleTooltip}></div>
                     </header>
                 </>
             )
@@ -76,13 +78,15 @@ function Header({ type, title }) {
                             <Button onClick={navigateBack} icon={<FontAwesomeIcon icon="fa-solid fa-arrow-left-long" size='xl' />}></Button>
                             <h1 className={`text-lg truncate ${isSearchBarHidden ? '' : 'hidden'}`}>{title}</h1>
                         </div>
-                        <input type="text" onChange={(e) => setSearchQuery(e.target.value)} placeholder='Search' className={`w-full px-4 ${isSearchBarHidden ? 'hidden' : ''} bg-white h-8 rounded-sm text-(--text-primary) text-base outline-none`} />
+                        <input type="text" onChange={(e) => setSearchQuery(e.target.value)} placeholder='Search' className={`w-full z-2 px-4 ${isSearchBarHidden ? 'hidden' : ''} bg-white h-8 rounded-sm text-(--text-primary) text-base outline-none`} />
+                        <div className={`${isSearchBarHidden ? 'hidden' : ''} backdrop absolute w-full h-full left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-1`} onClick={toggleSearchBar}></div>
                         <div className='flex'>
                             <Button onClick={toggleSearchBar} className="cursor-pointer" icon={<FontAwesomeIcon icon="fa-solid fa-magnifying-glass" size='xl' />} />
                             <Button onClick={toggleTooltip} className="cursor-pointer" icon={<FontAwesomeIcon icon="fa-solid fa-ellipsis-vertical" size='xl' />} />
                         </div>
                     </header>
                     <TooltipSubject className={`${isTooltipHidden ? 'hidden' : ''}`}></TooltipSubject>
+                    <div className={`${isTooltipHidden ? 'hidden' : ''} backdrop absolute w-full h-full left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-1`} onClick={toggleTooltip}></div>
                 </>
             )
 
