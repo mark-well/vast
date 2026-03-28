deb88ddf19177a29eb8f77c5662f9ef47b7f5924
mark-well <lacemerto05@gmail.com>
2026-03-28 18:19:20 +0800

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 import Home from './views/Home'
 import Subject from './views/Subject'
